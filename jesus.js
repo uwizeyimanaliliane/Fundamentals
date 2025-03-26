6f84b068776887e91677a45344db70e1b9c5c226
MUGANZA Jesus <0uganz@gmail.com>
2025-03-26 11:16:56 +0200

    MDN string method charAt

---
+++ jesus.js
@@ -0,0 +1,6 @@
+//charAt MDN string method
+
+let message = 'Welcome to solvit Africa';
+let method = message.charAt(4);// this mrthod will return the character at the specified index in a string
+
+console.log (method);
